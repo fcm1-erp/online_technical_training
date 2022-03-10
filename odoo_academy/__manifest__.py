dbb6c8f94ed1548284deb41fa75f5013f4445a2d
fc1m-erp <fernando.ceron@m1-erp.com>
2022-03-10 21:58:39 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- cofing: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'descrption': """
+        Acedemy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees    
+    """,
+    'author': 'Odoo',
+    'website': 'http://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+    'demo': [
+        
+    ],
+    
+}
